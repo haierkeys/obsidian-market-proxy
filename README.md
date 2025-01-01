4b327a600359551dab76f4dbafe6856fa5e49799
HaierKeys <haierkeys@gmail.com>
2025-01-01 12:18:28 +0800

    更新

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 
 
-# Obsidian 代理插件/Github代理
+# Obsidian Github Proxy / Github代理插件
 
-因为某些原因，在国内经常无法下载 Obsidian 的社区插件。这个项目的主要目的就是修复这种情况，让国内的用户也可以无障碍的下载社区插件。
-
-
-## Changelog & Todo
-- [x] 去除无法使用的镜像站
-- [x] 增加新的可用镜像站 验证时间 2023.10.13
-- [x] 增加自动关联镜像列表(之前是手动在代码里一个一个添加)
+因为某些原因，在国内无法下载更新访问 Obsidian 插件商店和主题商店的问题，
+本插件的主要目的就是修复这种情况，让国内的用户也可以无障碍使用。
 
 
 ### 上手指南
 
-1. 下载 [obsidian-proxy-github.zip](https://gitee.com/juqkai/obsidian-proxy-github/releases)
+1. 下载压缩包
 2. 解压 obsidian-proxy-github.zip
 3. 将解压的文件夹放入笔记目录下的插件目录内。如：XXX/.obsidian/plugins
 
@@ -22,6 +17,4 @@
 
 该项目签署了 Apache License 2.0 授权许可
 
-### 鸣谢
 
-- https://github.com/juqkai/obsidian-proxy-github
--- main.js
+++ main.js
@@ -1,278 +1,209 @@
-const { Plugin, PluginSettingTab, Setting } = require("obsidian");
+const { Plugin, PluginSettingTab, Setting } = require("obsidian")
 
-let server = 'ghproxy'
+let server = "github_diybeta_proxy"
 
-let proMap = {
-    ghproxy: {
-        down: "https://ghproxy.com/https://github.com/"
-        , raw: "https://ghproxy.com/https://raw.githubusercontent.com/"
-        , home: "https://ghproxy.com/https://github.com/"
-    },
-    "h.api.99988866.xyz": {
-        down: "https://gh.api.99988866.xyz/https://github.com/"
-        , raw: "https://gh.api.99988866.xyz/https://raw.githubusercontent.com/"
-        , home: "https://gh.api.99988866.xyz/https://github.com/"
-    },
+const proMap = {
+  github_diybeta_proxy: {
+    down: "https://github.diybeta.com/https://github.com/",
+    raw: "https://github.diybeta.com/https://raw.githubusercontent.com/",
+    home: "https://github.diybeta.com/https://github.com/",
+  },
 }
 
-let include = [
-    {
-        match: (url) => url.startsWith("https://github.com/") > 0 && url.indexOf("/releases/download/") >= 0
-        , to: (url) => url.replace("https://github.com/", proMap[server].down)
-    }
-    , {
-        match: (url) => url.startsWith("https://raw.githubusercontent.com/") > 0
-        , to: (url) => url.replace("https://raw.githubusercontent.com/", proMap[server].raw)
-    }
-    , {
-        match: (url) => url.startsWith("https://github.com/") > 0
-        , to: (url) => url.replace("https://github.com/", proMap[server].home)
-    }
+const include = [
+  {
+    match: (url) => url.startsWith("https://github.com/") && url.indexOf("/releases/download/") >= 0,
+    to: (url) => url.replace("https://github.com/", proMap[server].down),
+  },
+  {
+    match: (url) => url.startsWith("https://raw.githubusercontent.com/"),
+    to: (url) => url.replace("https://raw.githubusercontent.com/", proMap[server].raw),
+  },
+  {
+    match: (url) => url.startsWith("https://github.com/"),
+    to: (url) => url.replace("https://github.com/", proMap[server].home),
+  },
 ]
 
 // 匹配URL
 function matchUrl(e) {
-    console.log("原始访问：" + JSON.stringify(e))
-    for (var key in include) {
-        let item = include[key]
-        // console.log(key)
-        // console.log(item)
-        if (e && e.url && item.match(e.url)) {
-
-            e.url = item.to(e.url)
-            console.log("实际访问：" + JSON.stringify(e))
-            if (!e.headers) {
-                e.headers = {}
-            }
-            e.headers["content-type"] = "application/x-www-form-urlencoded";
-            e.headers["Access-Control-Allow-Origin"] = "*"
-            return true;
-        }
+  console.log("原始访问：" + JSON.stringify(e))
+  for (const item of include) {
+    if (e && e.url && item.match(e.url)) {
+      e.url = item.to(e.url)
+      console.log("实际访问：" + JSON.stringify(e))
+      if (!e.headers) {
+        e.headers = {}
+      }
+      e.headers["content-type"] = "application/x-www-form-urlencoded"
+      e.headers["Access-Control-Allow-Origin"] = "*"
+      return true
     }
-    return false;
+  }
+  return false
 }
 
 // 代理访问
 function proxy(e) {
-    return new Promise((function (t, n) {
-        e.success = t;
-        e.error = function (e, t) {
-            return n(t)
-        }
-        //debugger
-        if (app.isMobile) {
-            forMobile(e);
-            return;
-        }
-        forPC(e)
-    }))
+  return new Promise((resolve, reject) => {
+    e.success = resolve
+    e.error = (error, t) => reject(t)
+    if (app.isMobile) {
+      forMobile(e)
+    } else {
+      forPC(e)
+    }
+  })
 }
 
-/**
- *
- * https://github.com/denolehov/obsidian-git/issues/57
- * https://capacitorjs.com/blog/bypassing-cors-with-the-http-plugin
- * https://github.com/capacitor-community/http
- *
- * @param {*} e
- */
 async function forMobile(e) {
-    try {
-        const http = require('@capacitor-community/http')
-        const options = { url: e.url };
-        new window.Notice("发送请求：" + e.url, 10000)
-        new window.Notice(JSON.stringify(http.get) + "123", 10000)
-        // http.get(options);
-        const resp = await http.get(options).then((resp) => {
-            new window.Notice("请求成功：", 10000)
-            new window.Notice("请求成功：" + resp.status, 10000)
-            e.success(resp.data)
-        }).catch((error) => {
-            new window.Notice("出错了：" + JSON.stringify(error), 10000)
-        })
-        new window.Notice("请求成功12", 10000)
-        e.success(resp.data)
-
-    } catch (e) {
-        e.error(e)
-        new window.Notice("加载@capacitor-community/http出错", 10000)
-    }
+  try {
+    const http = require("@capacitor-community/http")
+    const options = { url: e.url }
+    new window.Notice("发送请求：" + e.url, 10000)
+    const resp = await http.get(options)
+    new window.Notice("请求成功：" + resp.status, 10000)
+    e.success(resp.data)
+  } catch (error) {
+    e.error(error)
+    new window.Notice("加载@capacitor-community/http出错", 10000)
+  }
 }
 
 function forPC(e) {
-    try {
-        const https = require('https')
-        https.get(e.url, function (res) {
-            new window.Notice("https.get成功", 10000)
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => {
-                rawData += chunk;
-            });
-            res.on('end', () => {
-                try {
-                    new window.Notice("https.get处理数据成功", 10000)
-                    e.success(rawData)
-                } catch (e) {
-                    new window.Notice("https.get处理数据失败", 10000)
-                    e.error(e)
-                }
-            });
-
-        }).on('error', function (e) {
-            new window.Notice("https.get失败", 10000)
-            e.error(e)
-        })
-    } catch (e) {
-        new window.Notice("导入http出错", 10000)
-        new window.Notice(JSON.parse(e), 10000)
-    }
+  try {
+    const https = require("https")
+    https.get(e.url, (res) => {
+      new window.Notice("https.get成功", 10000)
+      res.setEncoding("utf8")
+      let rawData = ""
+      res.on("data", (chunk) => {
+        rawData += chunk
+      })
+      res.on("end", () => {
+        try {
+          new window.Notice("https.get处理数据成功", 10000)
+          e.success(rawData)
+        } catch (error) {
+          new window.Notice("https.get处理数据失败", 10000)
+          e.error(error)
+        }
+      })
+    }).on("error", (error) => {
+      new window.Notice("https.get失败", 10000)
+      e.error(error)
+    })
+  } catch (error) {
+    new window.Notice("导入http出错", 10000)
+    new window.Notice(JSON.stringify(error), 10000)
+  }
 }
 
 function apProxy() {
-    var ap;
-    this.regedit = function () {
-        ap = window.ajaxPromise;
-        window.ajaxPromise = function (e) {
-
-            if (eisURL(e)) {
-                if (!matchUrl(e)) {
-                    return ap(e);
-                }
-            }
-
-            // new window.Notice("正在通过 ProxyGithub 来代理访问社区插件！")
-            return proxy(e)
+  let ap
+  this.regedit = function () {
+    ap = window.ajaxPromise
+    window.ajaxPromise = function (e) {
+      if (eisURL(e)) {
+        if (!matchUrl(e)) {
+          return ap(e)
         }
+      }
+      return proxy(e)
     }
-    this.unRegedit = function () {
-        window.ajaxPromise = ap;
-    }
+  }
+  this.unRegedit = function () {
+    window.ajaxPromise = ap
+  }
 }
 
-//window.Capacitor.registerPlugin("App").request
 function apCapacitor() {
-    var ap;
-    this.regedit = function () {
-        ap = window.Capacitor.registerPlugin("App").request;
-        console.log(ap)
-        window.Capacitor.registerPlugin("App").request = function (e) {
-            if (eisURL(e)) {
-                matchUrl(e);
-            }
-            //new window.Notice("正在通过 ProxyGithub 来代理访问社区插件！")
-            ap(e);
-            // if (matchUrl(e)) {
-            //     return ap(e);
-            // }
-        }
-        console.log("apc注册成功")
+  let ap
+  this.regedit = function () {
+    ap = window.Capacitor.registerPlugin("App").request
+    window.Capacitor.registerPlugin("App").request = function (e) {
+      if (eisURL(e)) {
+        matchUrl(e)
+      }
+      ap(e)
     }
-    this.unRegedit = function () {
-        window.window.Capacitor.registerPlugin("App").request = ap;
-    }
+    console.log("apc注册成功")
+  }
+  this.unRegedit = function () {
+    window.Capacitor.registerPlugin("App").request = ap
+  }
 }
 
 function eisURL(e) {
-    if (typeof e == "undefined") {
-        return false;
-    }
-    if (typeof e.url == "undefined") {
-        return false;
-    }
-
-    if (include[1].match(e.url) || include[2].match(e.url)) {
-        return true;
-    } else {
-        return false;
-    }
-
+  if (!e || !e.url) {
+    return false
+  }
+  return include.some(item => item.match(e.url))
 }
 
 function apElectron() {
-    var ap;
-    this.regedit = function () {
-        ap = window.require("electron").ipcRenderer.send;
-        //debugger
-        console.log(ap)
-        window.require("electron").ipcRenderer.send = function (a, b, e, ...rest) {
-            //debugger
-            // matchUrl(e);
-            if (eisURL(e)) {
-                matchUrl(e);
-            }
-
-            //new window.Notice("正在通过 ProxyGithub 来代理访问社区插件！")
-            ap(a, b, e, ...rest);
-            // if (matchUrl(e)) {
-            //     return ap(e);
-            // }
-        }
-        console.log("apc注册成功")
+  let ap
+  this.regedit = function () {
+    ap = window.require("electron").ipcRenderer.send
+    window.require("electron").ipcRenderer.send = function (a, b, e, ...rest) {
+      if (eisURL(e)) {
+        matchUrl(e)
+      }
+      ap(a, b, e, ...rest)
     }
-    this.unRegedit = function () {
-        window.require("electron").ipcRenderer.send = ap;
-    }
+    console.log("apc注册成功")
+  }
+  this.unRegedit = function () {
+    window.require("electron").ipcRenderer.send = ap
+  }
 }
-
-
-
 
 class ProxyGithubSettingTab extends PluginSettingTab {
-
-    constructor(app, plugin) {
-        console.log("加载了tab~~~~~~~~~~~~~~~~~~~~~~")
-        super(app, plugin)
-        this.plugin = plugin
-    }
-    async display() {
-        this.containerEl.empty()
-        new Setting(this.containerEl)
-            .setName('代理服务器')
-            .setDesc(`通过选择不同的服务器来切换代理，可以解决某些情况下，某个服务器无法访问的情况。当前代理服务器：${this.plugin.settings.server}`)
-            // .setValue(this.plugin.settings.server) // <-- Add me!
-            .addDropdown(dropDown => {
-                dropDown.addOption(server, '请选择');
-                for (let one in proMap) {
-                    dropDown.addOption(one, one);
-                }
-                dropDown.onChange(async (value) => {
-                    this.plugin.settings.server = value
-                    // this.plugin.settings.server = value;
-                    await this.plugin.saveSettings();
-                });
-            });
-    }
+  constructor(app, plugin) {
+    super(app, plugin)
+    this.plugin = plugin
+  }
+  async display() {
+    this.containerEl.empty()
+    new Setting(this.containerEl)
+      .setName("代理服务器")
+      .setDesc(`通过选择不同的服务器来切换代理，可以解决某些情况下，某个服务器无法访问的情况。当前代理服务器：${this.plugin.settings.server}`)
+      .addDropdown((dropDown) => {
+        dropDown.addOption(server, "请选择")
+        for (const one in proMap) {
+          dropDown.addOption(one, one)
+        }
+        dropDown.onChange(async (value) => {
+          this.plugin.settings.server = value
+          await this.plugin.saveSettings()
+        })
+      })
+  }
 }
 
-
-
-
-let app = new apProxy();
-let apc = new apCapacitor();
-let ape = new apElectron();
+const app = new apProxy()
+const apc = new apCapacitor()
+const ape = new apElectron()
 module.exports = class ProxyGithub extends Plugin {
-    onload() {
-        // new window.Notice("添加 ProxyGithub 代理访问社区插件！");
-        this.addSettingTab(new ProxyGithubSettingTab(this.app, this));
-        ape.regedit();
-        apc.regedit();
-        app.regedit();
-        this.settings = { server: server }
-    }
-    async loadSettings() {
-        this.settings = Object.assign({}, { server: server }, await this.loadData());
-    }
-    async saveSettings() {
-        await this.saveData(this.settings);
-        server = this.settings.server;
-        //debugger
-    }
+  async onload() {
+    this.addSettingTab(new ProxyGithubSettingTab(this.app, this))
+    ape.regedit()
+    apc.regedit()
+    app.regedit()
+    this.settings = { server: server }
+    await this.loadSettings()
+  }
+  async loadSettings() {
+    this.settings = Object.assign({}, { server: server }, await this.loadData())
+  }
+  async saveSettings() {
+    await this.saveData(this.settings)
+    server = this.settings.server
+  }
 
-    onunload() {
-        ape.unRegedit()
-        apc.unRegedit()
-        app.unRegedit()
-    }
+  onunload() {
+    ape.unRegedit()
+    apc.unRegedit()
+    app.unRegedit()
+  }
 }
-
