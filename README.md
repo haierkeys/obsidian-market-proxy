20e7f20eaa5b1f7264326b0f47d9b763e360db39
HaierKeys <haierkeys@gmail.com>
2025-01-02 17:28:38 +0800

    更新第一版

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
+# obsidian-market-proxy 插件主题市场代理插件 For Obsidian
 
-
-# Obsidian Github Proxy / Github代理插件
-
-本插件提供Obsidian插件和主题商店的网络代理功能，旨在解决中国大陆用户无法直接下载和更新插件与主题商店资源的问题，让国内用户能够顺畅使用。
-
+为 Obsidian 插件市场和 主题市场 提供网络代理功能，旨在解决中国大陆用户插件市场和主题市场无法使用的问题,支持PC和移动端。
 
 ### 上手指南
 
 1. 下载压缩包
-2. 解压 obsidian-proxy-github.zip
+2. 解压 obsidian-market-proxy.zip
 3. 将解压的文件夹放入笔记目录下的插件目录内。如：XXX/.obsidian/plugins
+4. 在 Obsidian 的设置中启用插件
 
 
-### 版权说明
-
-该项目签署了 Apache License 2.0 授权许可
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,140 +1,84 @@
-import { App, Plugin, PluginSettingTab, Setting, Platform } from "obsidian"
+import { Plugin, Platform } from "obsidian"
 
-
-
-
-let sendHandle: any
 class AppProxy {
   public sendHandle: any
 
   constructor() {
-    sendHandle = (window as any).electron.ipcRenderer.send
+    ;(window as any).sendHandle = (window as any).electron.ipcRenderer.send
   }
 
-  reg(plugin: GithubProxy) {
-    console.log("注册app代理")
+  register() {
     ;(window as any).electron.ipcRenderer.send = function (a: any, b: any, e: any, ...rest: any) {
-      if (eisURL(e, plugin)) {
-        matchUrl(e, plugin)
-      }
-      console.log("app代理", a, b, e, rest)
-      sendHandle(a, b, e, ...rest)
+      UrlRewrite(e)
+      ;(window as any).sendHandle(a, b, e, ...rest)
     }
   }
 
-  unReg(plugin: GithubProxy) {
-    console.log("注销app代理")
-    ;(window as any).electron.ipcRenderer.send = sendHandle
+  unRegister() {
+    ;(window as any).electron.ipcRenderer.send = (window as any).sendHandle
   }
 }
 
-
-
-export default class GithubProxy extends Plugin {
-  settings: PluginSettings
-  appProxy: AppProxy
-
-  proxyServers: { [key: string]: { down: string; raw: string; home: string } } = {
-    github_diybeta_proxy: {
-      down: "https://github.diybeta.com/https://github.com/",
-      raw: "https://github.diybeta.com/https://raw.githubusercontent.com/",
-      home: "https://github.diybeta.com/https://github.com/",
-    },
+class NativeProxy {
+  constructor() {
+    ;(window as any).sendHandle = (window as any).Capacitor.toNative
+  }
+  register() {
+    ;(window as any).toNativeContainer = function () {
+      ;(window as any).Capacitor.toNative = (a: any, b: any, c: any, d: any) => {
+        if (a == "App" && b == "requestUrl") {
+          UrlRewrite(c)
+        }
+        return (window as any).sendHandle(a, b, c, d)
+      }
+    }
+    ;(window as any).toNativeContainer()
   }
 
-  proxyReplace: { match: (url: string) => boolean; to: (url: string) => string }[] = [
-    {
-      match: (url: string) => url.startsWith("https://github.com/") && url.indexOf("/releases/download/") >= 0,
-      to: (url: string) => url.replace("https://github.com/", this.proxyServers[this.settings.proxyServer].down),
-    },
-    {
-      match: (url: string) => url.startsWith("https://raw.githubusercontent.com/"),
-      to: (url: string) => url.replace("https://raw.githubusercontent.com/", this.proxyServers[this.settings.proxyServer].raw),
-    },
-    {
-      match: (url: string) => url.startsWith("https://github.com/"),
-      to: (url: string) => url.replace("https://github.com/", this.proxyServers[this.settings.proxyServer].home),
-    },
-  ]
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
-  }
-  async saveSettings(isStatusCheck: boolean = true) {
-    await this.saveData(this.settings)
-  }
-
-  async onload() {
-    await this.loadSettings()
-    // 注册设置选项
-    this.addSettingTab(new GithubProxySettingTab(this.app, this))
-
-    this.appProxy = new AppProxy()
-    this.appProxy.reg(this)
-  }
-
-  onunload() {
-    this.appProxy.unReg(this)
+  unRegister() {
+    ;(window as any).Capacitor.toNative = (window as any).sendHandle
   }
 }
 
-function eisURL(e: any, plugin: GithubProxy): boolean {
+function isNeedUrlRewrite(e: any): boolean {
   if (!e || !e.url) {
     return false
   }
-  return plugin.proxyReplace.some((item: any) => item.match(e.url))
-}
-
-function matchUrl(e: any, plugin: GithubProxy): boolean {
-  console.log("原始访问：" + JSON.stringify(e))
-  for (const item of plugin.proxyReplace) {
-    if (e && e.url && item.match(e.url)) {
-      e.url = item.to(e.url)
-      console.log("实际访问：" + JSON.stringify(e))
-      if (!e.headers) {
-        e.headers = {}
-      }
-      e.headers["content-type"] = "application/x-www-form-urlencoded"
-      e.headers["Access-Control-Allow-Origin"] = "*"
-      return true
-    }
+  if (e.url.startsWith("https://github.com/") || e.url.startsWith("https://raw.githubusercontent.com/")) {
+    return true
   }
   return false
 }
 
-export interface PluginSettings {
-  proxyServer: string
-}
+function UrlRewrite(e: any): void {
+  if (e && e.url && isNeedUrlRewrite(e)) {
+    const src = e.url
+    e.url = "https://github.diybeta.com/" + e.url
 
-export const DEFAULT_SETTINGS: PluginSettings = {
-  // 是否自动上传
-  proxyServer: "",
-}
-
-class GithubProxySettingTab extends PluginSettingTab {
-  plugin: GithubProxy
-
-  constructor(app: App, plugin: GithubProxy) {
-    super(app, plugin)
-    this.plugin = plugin
+    if ((window as any).Capacitor.isLoggingEnabled) {
+      window.console.info(JSON.stringify({ src: src, proxy: e.url }))
+    }
+    if (!e.headers) {
+      e.headers = {}
+    }
+    e.headers["content-type"] = "application/x-www-form-urlencoded"
+    e.headers["Access-Control-Allow-Origin"] = "*"
   }
-  display(): void {
-    const { containerEl: set } = this
+}
 
-    set.empty()
+export default class MarketProxy extends Plugin {
+  proxy: AppProxy | NativeProxy
 
-    new Setting(set)
-      .setName("代理服务器")
-      .setDesc(`通过选择不同的服务器来切换代理，可以解决某些情况下，某个服务器无法访问的情况。当前代理服务器：`)
-      .addDropdown((dropDown: any) => {
-        dropDown.addOption(this.plugin.settings.proxyServer, "请选择")
-        for (const one in this.plugin.proxyServers) {
-          dropDown.addOption(one, one)
-        }
-        dropDown.onChange(async (value: any) => {
-          this.plugin.settings.proxyServer = value
-          await this.plugin.saveSettings()
-        })
-      })
+  async onload() {
+    if (Platform.isDesktop) {
+      this.proxy = new AppProxy()
+    } else {
+      this.proxy = new NativeProxy()
+    }
+    this.proxy.register()
+  }
+
+  onunload() {
+    this.proxy.unRegister()
   }
 }
